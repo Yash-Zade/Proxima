b51cb3b4a71226cec10747a37496c88f1e748fd7
Yash Zade <yashmzade@gmail.com>
2025-02-22 13:17:14 +0530

    Initial setup and basic auth features

---
+++ src/main/java/com/teamarc/proxima/ProximaApplication.java
@@ -0,0 +1,13 @@
+package com.teamarc.proxima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProximaApplication.class, args);
+    }
+
+}
