95aab4d70f70b70e8b6c87f5f2f44bee64f88e26
Yash Zade <yashmzade@gmail.com>
2025-02-22 13:46:11 +0530

    Add Entity and DTO's

---
+++ src/main/java/com/teamarc/proxima/entity/College.java
@@ -0,0 +1,29 @@
+package com.teamarc.proxima.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class College {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+    private String website;
+
+    @OneToMany(mappedBy = "college", cascade = CascadeType.ALL)
+    private List<Student> students;
+}
